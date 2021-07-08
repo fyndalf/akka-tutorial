3c2d30d06f3f9e2da450656b492ec4a8e53f2138
Finn K <fyndalf@users.noreply.github.com>
2021-07-08 20:27:29 +0200

    Filter for non-empty inds

--- assignment4/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ assignment4/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -36,7 +36,7 @@ object Sindy {
     val inclusionLists = attributeSets.select(explode(col("attributeSet")).as("key"), col("attributeSet")).as[(String, Seq[String])]
       .map(row => (row._1, row._2.filter(a => !a.equals(row._1)))).filter(row => row._2.nonEmpty).sort(col("_1"))
 
-    val inds = inclusionLists.groupByKey(t => t._1).mapGroups((g, h) => (g, h.map(_._2).reduce(_.intersect(_)))).sort("_1")
+    val inds = inclusionLists.groupByKey(t => t._1).mapGroups((g, h) => (g, h.map(_._2).reduce(_.intersect(_)))).filter(row => row._2.nonEmpty).sort("_1")
 
     inds.collect().foreach(row => println(row._1 + " < " + row._2.sorted.reduce(_ + ", " + _)))
 
