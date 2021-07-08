00cc009511980abc3ad04f8dd3020b98063295d5
Finn K <fyndalf@users.noreply.github.com>
2021-07-08 19:21:57 +0200

    Remove debug output and fix implementation

--- assignment4/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
+++ assignment4/src/main/scala/de/hpi/spark_tutorial/Sindy.scala
@@ -1,7 +1,7 @@
 package de.hpi.spark_tutorial
 
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.sql.functions.{col, collect_set, explode, size}
+import org.apache.spark.sql.functions.{col, collect_set, explode}
 
 object Sindy {
 
@@ -27,30 +27,18 @@ object Sindy {
       }).toDF()
     })
 
-    cells.foreach(_.show(false))
-
     val groupedCells = cells.reduce((cell1, cell2) => cell1.join(cell2, cell2.columns, joinType = "fullouter"))
-
-    groupedCells.show(false)
 
     val groupAndAggregated = groupedCells.groupBy("_1").agg(collect_set("_2").as("attributeSet"))
 
-    groupAndAggregated.show(false)
-
     val attributeSets = groupAndAggregated.select("attributeSet")
-    attributeSets.show(false)
-
-    //val inclusionLists = attributeSets.flatMap(row => row.toSeq.map(e => (e.toString, row.toSeq.filter(a => !a.toString.equals(e.toString)).map(_.toString))))
-
 
     val inclusionLists = attributeSets.select(explode(col("attributeSet")).as("key"), col("attributeSet")).as[(String, Seq[String])]
       .map(row => (row._1, row._2.filter(a => !a.equals(row._1)))).filter(row => row._2.nonEmpty).sort(col("_1"))
-    inclusionLists.show(numRows = 500)
 
-    val inds = inclusionLists.groupBy("_1").agg(collect_set("_2").as("superSets")).sort()
-    inds.show(false)
+    val inds = inclusionLists.groupByKey(t => t._1).mapGroups((g, h) => (g, h.map(_._2).reduce(_.intersect(_)))).sort("_1")
 
-    inds.collect().foreach(row => println(row.get(0) + " < " + row.get(1).asInstanceOf[Seq[String]].sorted.reduce(_ + ", " + _)))
-    
+    inds.collect().foreach(row => println(row._1 + " < " + row._2.sorted.reduce(_ + ", " + _)))
+
   }
 }
